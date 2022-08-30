6ae98dc973c9d7ddf90187a80dfae979bc41918b
Mauricio Araque <111613206+araquester@users.noreply.github.com>
2022-08-29 19:58:38 -0500

    Ejercicio 1 Ejemplo For

---
+++ Cuadrado_Numeros.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que calcule el cuadrado de los 9 primeros números naturales e
+imprima por pantalla el número seguido de su cuadrado. Ejemplo: “2 elevado al cuadrado
+es igual a 4”, y así sucesivamente.
+"""
+for i in range(1,10):
+    print(f"{i} Elevado al cuadrado es igual a ",i**2)
